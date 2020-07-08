881b18d147a52633771d528761e53136dfed865d
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-08 19:39:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 def countdown(number)
-  number = 10
+  #number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
@@ -8,10 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  number = 10
+  #number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep(0.5)
+    sleep(1)
     number -= 1
   end
   "HAPPY NEW YEAR!"
