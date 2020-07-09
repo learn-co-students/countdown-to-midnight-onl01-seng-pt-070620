b912f3ddeac76ed8ba657dea2e562256f7e40154
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-09 22:45:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(num_sec)
+   while num_sec > 0
+   puts "#{num_sec} SECOND(S)!" 
+   num_sec -= 1
+ end
+ "HAPPY NEW YEAR!"
+ end
 
-def countdown
+def countdown_with_sleep(num_sec)
+  while num_sec > 0
+  puts "#{num_sec} SECOND(S)!"
+  sleep(1)
+  num_sec -= 1
 end
+"HAPPY NEW YEAR"
+end
+
