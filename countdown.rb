84bea99d5b0f5784a62ec1e189555dcb196d94b3
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 01:58:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 
 def countdown(number)
   
-  number=10
-  while number > 1 
+  while number > 0
  puts "#{number} SECOND(S)!"
  number -= 1
- return "HAPPY NEW YEAR!"
+ 
 end
+return "HAPPY NEW YEAR!"
 end
